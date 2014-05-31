056c0c6a2e94ac538dd5f8f91577f31830c96377
patogeno <patosoto@gmail.com>
2014-05-31 11:08:41 +0800

    App that uses stylus and nib

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
+//Module Dependencies
 var express = require('express');
+var stylus = require('stylus');
+var nib = require('nib');
+var morgan = require('morgan');
 
+//Set port number
+var portnumber = 3000;
+
+//Initialise Express
 var app = express();
+console.log('Express has been initialized');
 
+function compile(str, path) {
+    return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+
+//Set Views Folder
+app.set('views',__dirname + '/views');
+
+//Initialise Jade
+app.set('view engine', 'jade');
+console.log('Jade has been initialised');
+
+//Stylus Middleware
+app.use(morgan('dev'));
+app.use(stylus.middleware(
+    {
+        src: __dirname + '/public',
+        compile: compile
+    }
+));
+app.use(express.static(__dirname + '/public'));
+
+//Render Index Page
 app.get('/', function(req, res) {
-    res.render('index.ejs');
+    res.render('index',
+               {title: 'Welcome'}
+              );
 });
 
-app.get('/about', function(req, res) {
-    res.render('layout.ejs',{
-        title: 'About Us',
-        body:'<h1>About Usss</h1>'
-    });
-});
-
-app.get('/*', function(req, res) {
-    res.status(404).render('error.ejs');
-});
-
-console.log('Server started');
-
-app.listen(3000);
+app.listen(portnumber);
+console.log('Server is now running on port ' + portnumber);
